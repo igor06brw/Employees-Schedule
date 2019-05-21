a96a5281879036930c00aed3fdd156d28923413c
Igor Borowski <igor06brw@gmail.com>
2019-05-21 20:50:27 +0200

    watchThis function repaired & it's work

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,52 @@
 const gulp = require('gulp');
-const { src, dest } = require('gulp');
-const { series, parallel } = require('gulp');
+const { watch, series, parallel } = require('gulp');
 const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-
+const browserSync = require('browser-sync').create();
 
 
 function styles() {
   return gulp.src('./src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dest/css'));
-};
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+    .pipe(gulp.dest('./dest/css'))
 
-function  sassWatch() {
-  gulp.watch('./src/sass/*.scss', ['sass']);            //
 };
 
 function scripts() {              
   return gulp.src('./src/*.js', { sourcemaps: true })
     .pipe(rename('main.min.js'))                        //rename to main.min.js
     .pipe(uglify())                                     //minify ES5+
-    .pipe(gulp.dest('./dest/js'));                      //finish to dest folder js
+    .pipe(gulp.dest('./dest/js'))                       //finish to dest folder js
+
 }
 
-let build = gulp.series(parallel(scripts, sass), sassWatch);
+// function syncBrowser() {
+//   browserSync.init(["./dest/css/*.css", "./dest/js/*.js"], {
+//     server: {
+//         baseDir: "./"
+//     }
+//   });
+// }
+
+function watchThis() {
+  watch('./src/sass/*', styles);       
+  watch('./src/scripts/*', scripts);    
+};
+
+gulp.task('default', function() {
+  styles();
+});
+// let build = gulp.series(parallel(scripts, sass), sassWatch);
 
 exports.styles = styles;
 exports.scripts = scripts;
-exports.sassWatch = sassWatch;
+exports.browserSync = syncBrowser;
 
-exports.default = build;
+exports.watchThis = watchThis;
